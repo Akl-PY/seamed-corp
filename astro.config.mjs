7fdafe64eff25fcd10e3b134f694f4c983e2f661
Akl-DGaletto <danielgenarog@gmail.com>
2026-09-14 17:00:54 -0300

    Add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
 import keystatic from "@keystatic/astro";
 
 import react from "@astrojs/react";
@@ -10,7 +11,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://seamed.com.ar",
-
+  output: "static",
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
